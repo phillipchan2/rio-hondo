795b5d3f4119283889930ad69f2a6827dcd87b34
phillipchan2 <phillip.chan@liferay.com>
2018-09-20 15:05:02 -0700

    Insert CSS into HTML pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const runSequence = require('run-sequence');
 const concat = require('gulp-concat');
+const fs = require('fs');
+const gulp = require('gulp');
+const insert = require('gulp-insert');
+const runSequence = require('run-sequence');
+const sass = require('gulp-sass');
+const styleInject = require('gulp-style-inject');
 
-gulp.task('sass', function() {
+gulp.task('sass', () => {
 	return gulp
 		.src('src/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
@@ -11,15 +14,25 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest('dist/css'));
 });
 
+gulp.task('pages', () => {
+	return gulp
+		.src('src/pages/**/*html')
+		.pipe(
+			insert.prepend(`<!-- inject-style src="./dist/css/app.min.css" -->`)
+		)
+		.pipe(styleInject())
+		.pipe(gulp.dest('dist/pages'));
+});
+
 // Watch for changes
-gulp.task('watch', function() {
+gulp.task('watch', () => {
 	// watch html files
-	// gulp.watch('client/**/*.html', ['refresh']);
+	gulp.watch('src/**/*.html', ['pages']);
 
 	// Watch .scss files
 	gulp.watch('src/**/*.scss', ['sass']);
 });
 
 gulp.task('default', function(callback) {
-	runSequence('sass', 'watch');
+	runSequence('sass', 'pages', 'watch');
 });
