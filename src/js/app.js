23b2c6d4157acf44a76c23778d222138c3fb81fd
Phillip Chan <phillip.chan@liferay.com>
2018-10-15 11:05:32 -0700

    Start staff module

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+// staff
+
+// read bio
+document.querySelectorAll('.read-bio-container').forEach(function(elem) {
+	elem.addEventListener('click', function(e) {
+		var staffContainer = e.target.parentNode.parentNode;
+		var toggleClassName = 'read-bio-active';
+
+		if (!staffContainer.classList.contains(toggleClassName)) {
+			staffContainer.classList.add(toggleClassName);
+		} else {
+			staffContainer.classList.remove(toggleClassName);
+		}
+	});
+});
+
+document.querySelectorAll('.contact-container').forEach(function(elem) {
+	elem.addEventListener('click', function(e) {
+		var staffContainer = e.target.parentNode.parentNode;
+		var toggleClassName = 'contact-active';
+
+		if (!staffContainer.classList.contains(toggleClassName)) {
+			staffContainer.classList.add(toggleClassName);
+		} else {
+			staffContainer.classList.remove(toggleClassName);
+		}
+	});
+});
